a592ae063d930c39e838081f3a9a3f996d90be71
CodeEclair <valeriia1belenko@gmail.com>
2024-10-22 16:32:58 +0300

    Initial Commit

---
+++ SnakeGameApp/SnakeGameAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnakeGameAppApp.swift
+//  SnakeGameApp
+//
+//  Created by Валерия Беленко on 22/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SnakeGameAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
